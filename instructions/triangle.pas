87b3b7e343d6792e5675fdd492557ec8f37e2571
Alex Kurkar <akurkar07@gmail.com>
2026-04-05 22:05:06 +0100

    Refactor triangle program to consolidate variable declarations and enhance readability; replace hardcoded limit with variable

--- instructions/triangle.pas
+++ instructions/triangle.pas
@@ -1,24 +1,33 @@
 PROGRAM triangle;
 
 VAR
-    n : INTEGER;
-    r : INTEGER;
-    
+    n, r, limit, spaces : INTEGER;
+
 FUNCTION factorial(n : INTEGER) : INTEGER;
-    BEGIN
-        IF n <= 1 THEN Factorial := 1
-        ELSE Factorial := n * Factorial(n - 1);
-    END;
+BEGIN
+    IF n <= 1 THEN
+        factorial := 1
+    ELSE
+        factorial := n * factorial(n - 1);
+END;
 
 FUNCTION choose(n, r : INTEGER) : INTEGER;
-    BEGIN
-        choose := factorial(n) DIV (factorial(n - r) * factorial(r));
-    END;
+BEGIN
+    choose := factorial(n) DIV (factorial(n - r) * factorial(r));
+END;
 
-BEGIN 
+BEGIN
+    limit := 10;
     n := 0;
-    WHILE n < 10 DO
+    WHILE n < limit DO
     BEGIN
+        spaces := 0;
+        WHILE spaces < limit - n DO
+        BEGIN
+            WRITE(' ');
+            spaces := spaces + 1;
+        END;
+
         r := 0;
         WHILE r <= n DO
         BEGIN
@@ -26,8 +35,8 @@ BEGIN
             WRITE(' ');
             r := r + 1;
         END;
+
         WRITELN('');
         n := n + 1;
     END;
 END.
-    
